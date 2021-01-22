ccdd0a68aff87b4d020db9c43e0f45d895aa326d
Hussain Ali <ali.415code@gmail.com>
2021-01-22 11:08:36 -0800

    updated

--- src/components/Smurf.js
+++ src/components/Smurf.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 class Smurf extends React.Component {
     render() {
-        const { smurf } = this.props;
+        const { smurfs } = this.props;
 
-        return(<div data-testid="smurf" className="card">
+        return(
+        <div data-testid="smurf" className="card">
+            {smurfs.map((smurf, index) => (
+                <div key={index}>
+                    <h2>{smurf.name}</h2>
+                    <h3>"{smurf.nickname}"</h3>
+                    <h4>{smurf.position}</h4>
+                    <p>{smurf.description}</p>
+                </div>
+            ))}
         </div>);
     }
 }
