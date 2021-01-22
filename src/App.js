6799fe0fc6cc14b86a03f91a967bb8ea01d4dda2
Hussain Ali <ali.415code@gmail.com>
2021-01-22 11:40:06 -0800

    updated MVP

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   componentDidMount() {
     axios.get('http://localhost:3333/smurfs').then(res=>{
-      console.log(res.data);
+      console.log(res);
     })
   }
   
